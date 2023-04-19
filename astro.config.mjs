58162b138aafe398042fa716f7bfd83c6a5e2892
jamesqquick <james.q.quick@gmail.com>
2023-04-19 11:54:59 -0500

    Updates site info

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
-import svelte from "@astrojs/svelte";
+import tailwind from '@astrojs/tailwind';
+import image from '@astrojs/image';
+import svelte from '@astrojs/svelte';
 
-import netlify from "@astrojs/netlify/functions";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://astrocourse.dev',
   integrations: [mdx(), sitemap(), tailwind(), image(), svelte()],
-  output: "server",
-  adapter: netlify()
+  output: 'server',
+  adapter: netlify(),
 });
