e2a911a35caef174e48b1c4b93762eb08e0fa5bb
jamesqquick <james.q.quick@gmail.com>
2023-05-28 13:41:24 -0500

    first draft at redesign

--- src/components/FAQ.svelte
+++ src/components/FAQ.svelte
@@ -5,15 +5,9 @@
   let open = false;
 </script>
 
-<div
-  class="bg-gradient-to-r p-2 from-blue-800 to-purple-800 rounded-xl md:rounded-full lg:rounded-3xl"
->
-  <div
-    class="rounded-xl md:rounded-full lg:rounded-3xl px-6 md:px-20 py-8 lg:py-20 bg-blue-950 w-full h-full"
-  >
-    <p class="text-2xl lg:text-4xl font-bold text-purple-500 mb-2 lg:mb-6">
-      {faq.question}
-    </p>
-    <p class="text-white text-lg lg:text-xl tracking-wide">{faq.answer}</p>
-  </div>
+<div>
+  <h3 class="text-2xl lg:text-4xl font-bold mb-2 lg:mb-6">
+    {faq.question}
+  </h3>
+  <p class="text-white text-lg lg:text-xl tracking-wide">{faq.answer}</p>
 </div>
